b82d130695b5396a58195cc43d555e1fe9353d11
tsmith43 <38176921+tsmith43@users.noreply.github.com>
2019-12-10 02:50:41 -0500

    assests folder

---
+++ Scripts/LoadGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+
+    public void LoadScene(int level)
+    {
+        if (level < 3)
+        {
+            SceneManager.LoadScene(level);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
